5ffef6cdf3ac19c87d7d667f00d1ca85da52c826
chan <986632543@qq.com>
2018-10-26 02:01:29 +0800

    te

--- java8-lambda/src/main/java/io/github/biezhi/java8/lambda/lesson3/LambdasPractice.java
+++ java8-lambda/src/main/java/io/github/biezhi/java8/lambda/lesson3/LambdasPractice.java
@@ -1,6 +1,10 @@
 package io.github.biezhi.java8.lambda.lesson3;
 
+import io.github.biezhi.java8.lambda.lesson1.Project;
+
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class LambdasPractice {
     public static  String toLower(String s)
@@ -15,12 +19,19 @@ public class LambdasPractice {
     {
         return  s+"\n"+"China";
     }
-
+    public static void m1()
+    {
+        System.out.println(1);
+    }
     public static void main(String[] args) {
         Function<String,String> function=Function.identity();
         String result=function.andThen(LambdasPractice::addAuthor).andThen(LambdasPractice::addCountry)
                 .apply("test");
         System.out.print(result);
+        //方法为自身方法时，可以为无参数方法
+        Consumer<String> consumer=String::toLowerCase;
+        //方法为外部方法时，需返回值，参数值相匹配
+        Consumer<String> consumer1= System.out::print;
 
 
     }
